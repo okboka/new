4f991ea3894cb9e6f53beb73e267386fa4d8c74c
okboka <43502494+okboka@users.noreply.github.com>
2018-09-22 12:31:25 -0700

    first code

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+  public static void main (String [] args) {
+    String s = "Hello World!";
+    System.out.println(s); 
+    int number = 8;
+    int number2 = 3;
+    System.out.println((double)number/number2);
+    double num = 9.0;
+    double number3 = 8.8;
+    System.out.println(number3/num);
+    
+}
+}
